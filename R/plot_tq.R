303cf752738f8dbb094eb2840ed18048fc5fd441
lingjieliau <auroralau0909@gmail.com>
2019-12-19 13:56:02 -0500

    Add data track for abundance

--- R/plot_tq.R
+++ R/plot_tq.R
@@ -15,6 +15,13 @@
 #' @include transcript_quantifier-class.R
 #' @name plot_model
 #' @export
+
+chrom = 1
+start = 1
+end= 11000
+strand = NULL
+gene_name = NULL
+
 plot_model <- function(transcript_quantifier, gene_name = NULL,
                        chrom = NULL, start = NULL, end = NULL,
                        strand = NULL) {
@@ -22,10 +29,10 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
   tq <- transcript_quantifier
   # Check that only gene name or position information is specified
   if (!is.null(gene_name) & (!is.null(chrom) | ! is.null(start) |
-                            !is.null(end))) {
+                             !is.null(end))) {
     stop("Only gene name OR positional information can be specified")
   }
-
+  
   # Check for gene name specification
   if (!is.null(gene_name)) {
     gid_col <- tq@column_identifiers[2]
@@ -33,7 +40,7 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
       stop("transcript_quantifier object not built with gene ids")
     }
     if (!gene_name %in%
-       S4Vectors::elementMetadata(tq@transcripts)[, gid_col]) {
+        S4Vectors::elementMetadata(tq@transcripts)[, gid_col]) {
       stop(paste("Gene", gene_name, "does not exist"))
     }
   } else if (!is.null(chrom) & is.numeric(start) & is.numeric(end)) {
@@ -51,30 +58,30 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
   } else {
     stop("Incorrect positional specification")
   }
-
+  
   ## ** End checking **
-
+  
   # Get target transcripts
   target_tx <- get_transcripts(tq, gene_name, chrom, start, end, strand)
-
+  
   # Define bounds of plot range
   if (is.null(start)) {
     chrom <- GenomicRanges::seqnames(target_tx)[1]
     start <- min(GenomicRanges::start(target_tx))
     end <- max(GenomicRanges::end(target_tx))
   }
-
+  
   # Get masks
   tx_col <- tq@column_identifiers[1]
   gid_col <- tq@column_identifiers[2]
   transcripts <- S4Vectors::elementMetadata(target_tx)[, tx_col]
   target_masks <- get_masks(tq, transcripts)
   GenomicRanges::strand(target_masks) <- "*"
-
+  
   # Get count data
   data <- get_data(tq, chrom, start, end)
   data <- data[data$value > 0]
-
+  
   # Transcripts track
   if (!is.na(gid_col)) {
     gene_names <- S4Vectors::elementMetadata(target_tx)[, gid_col]
@@ -82,15 +89,18 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
     gene_names <- rep("placeholder", length(target_tx))
   }
   unique_gene_names <- unique(unlist(gene_names))
-
+  
   tx_track <- Gviz::AnnotationTrack(target_tx, name = "transcripts",
                                     id = transcripts,
                                     feature = unlist(tq@transcripts$gene_id))
-  Gviz::feature(tx_track)
-
+  
   # Masks track
   masks_track <- Gviz::AnnotationTrack(target_masks, name = "masks")
-
+  
+  # Abundance track
+  abund_track <- get_abund_track(tq, transcripts)
+  if (!is.null.(abund_track)) {print("yes")}
+  
   # Data plotting track
   d_track_plus <- NULL
   d_track_minus <- NULL
@@ -99,7 +109,7 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
                                     type = "h",
                                     name = "Summarized read counts (+)",
                                     col = "red"
-                                    )
+    )
   }
   if (length(data[GenomicRanges::strand(data) == "-"]) > 0) {
     data[GenomicRanges::strand(data) == "-"]$value <-
@@ -108,14 +118,15 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
                                      type = "h",
                                      name = "Summarized read counts (-)",
                                      col = "blue"
-                                     )
+    )
   }
-
+  
   # Plot tracks
-  args <- list(trackList = list(tx_track,
+  args <- list(trackList = c(list(tx_track,
                                 masks_track,
                                 d_track_plus,
                                 d_track_minus),
+                             abund_track),
                from = start, to = end,
                featureAnnotation = "feature")
   args$trackList <- #nolint
@@ -123,7 +134,7 @@ plot_model <- function(transcript_quantifier, gene_name = NULL,
   gene_color_list <- viridisLite::viridis(length(unique_gene_names))
   names(gene_color_list) <- unique_gene_names
   args <- c(args, gene_color_list)
-
+  
   return(do.call(Gviz::plotTracks, args))
 }
 
@@ -145,7 +156,7 @@ get_transcripts <- function(transcript_quantifier, gene_name = NULL,
     gid_col <- tq@column_identifiers[2]
     genes <- S4Vectors::elementMetadata(tq@transcripts)[, gid_col]
     out <- tq@transcripts[genes == gene_name]
-  } else {
+  } else { 
     query_range <- GenomicRanges::GRanges(chrom,
                                           IRanges::IRanges(start, end),
                                           strand)
@@ -175,6 +186,27 @@ get_masks <- function(transcript_quantifier, transcripts) {
     target_masks <- c(target_masks, tq@bins[[g]][tq@masks[[g]]])
   }
   return(target_masks)
+}
+
+get_abund_track <- function(transcript_quantifier, transcripts){
+  tq <- transcript_quantifier
+  key <- tq@transcript_model_key
+  target_group <- unique(key[key$tx_name %in% transcripts, ]$group)
+  for (g in target_group) {
+    target_models <- tq@models[[g]]
+    target_abund <- tq@model_abundance[[g]]
+    target_models_nrow <- nrow(tq@models[[g]])
+    target_bins <- tq@bins[[g]]
+    target_meta <- target_models * matrix(rep(target_abund, target_models_nrow),
+                                            nrow = target_models_nrow,
+                                            byrow = TRUE)
+    dtrack <- c(dtrack, Gviz::DataTrack(range = target_bins,
+                                        data = t(target_meta),
+                                        groups = names(target_abund),
+                                        type = "histogram"))
+  }
+  return(dtrack)
+  
 }
 
 #' @title Get data
@@ -211,7 +243,7 @@ methods::setMethod("get_data",
                      key <- data_source@transcript_model_key
                      target_group <-
                        unique(key[key$tx_name %in% transcripts, ]$group)
-
+                     
                      # Combine the data and the GRanges object
                      value_granges <- BiocGenerics::Reduce("c", mapply(
                        function(bins, counts) {
@@ -233,3 +265,4 @@ methods::setMethod("get_data",
                      stop("Not implemented")
                    }
 )
+
